eb05054190f65eb6e5d55ed6b214bd14b316fb4c
dhannyjsb <dhannyjsb@gmail.com>
2018-08-16 19:01:07 +0800

    Fix Reload Add Command Set Change Config.yml

--- src/online/inferniaserver/mc/main.java
+++ src/online/inferniaserver/mc/main.java
@@ -1,5 +1,8 @@
 package online.inferniaserver.mc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,43 +15,56 @@ import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin implements Listener,CommandExecutor{
 
-      String myLore = getConfig().getString("lore");
-      String ColorLore = ChatColor.translateAlternateColorCodes('&', myLore);
-      String msg = getConfig().getString("message-craft");
-      String MsgColor = ChatColor.translateAlternateColorCodes('&', msg);
-      String msgconsume = getConfig().getString("message-consume");
-      String msgconsumeColor = ChatColor.translateAlternateColorCodes('&', msgconsume);
-      String msgplace = getConfig().getString("message-use");
-      String msguseeColor = ChatColor.translateAlternateColorCodes('&', msgplace);
 	  @Override
 	  public void onEnable()
 	  {
+
 	    getServer().getPluginManager().registerEvents(this, this);
 	    saveDefaultConfig();
-	    System.out.print(ChatColor.AQUA+"Disable Lore Craft Enable");
+	    this.getServer().getConsoleSender().sendMessage(getConfig().getString("prefix").replace("&", "§") + " Disable Lore Craft Active");
 	  }
 	  
 	  public void onDisable() {
 		  this.saveConfig();
-	    System.out.print(ChatColor.DARK_RED+"Disable Lore Craft Disable");
+		    this.getServer().getConsoleSender().sendMessage(getConfig().getString("prefix").replace("&", "§") + " Disable Lore Craft Shutdown");
 	  }
 	  
-	    @Override
+	    @SuppressWarnings("deprecation")
+		@Override
 	    public boolean onCommand(CommandSender sender, Command command, String label, String[] arg) {
 	        if(command.getName().equalsIgnoreCase("disablelorecraft")){
 	            if(sender.hasPermission("disablelorecraft.admin")){
 	                if(arg.length < 1){
-	                    sender.sendMessage("disablelorecraft reload");
+	                    sender.sendMessage(getConfig().getString("prefix").replace("&", "§") + " /disablelorecraft reload");
+	                    sender.sendMessage(getConfig().getString("prefix").replace("&", "§") + " /disablelorecraft status");
+	                    sender.sendMessage(getConfig().getString("prefix").replace("&", "§") + " /disablelorecraft set");
 	                    return true;
 	                } if(arg[0].equals("reload")) {
-	                      this.saveDefaultConfig();
-	                      this.reloadConfig();
-	                    sender.sendMessage(ChatColor.RED + " plugin has been reloaded");
+	                      reloadConfig();
+	                    sender.sendMessage(getConfig().getString("prefix").replace("&", "§") + " Plugin has been reloaded");
+	        		    this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN+ getConfig().getString("prefix").replace("&", "§") + " Plugin has been reloaded");
 	                    return true;
+	                }
+	                if(arg[0].equals("status")) {
+	                    sender.sendMessage(getConfig().getString("prefix").replace("&", "§") + " Disable Lore: " + getConfig().getString("lore").replace("&", "§"));
+	                    return true;
+	                }
+	                if(arg[0].equals("set")) {
+	                    Player p = (Player)sender;
+	                    ItemStack is = p.getItemInHand();
+	                    ItemMeta im = is.getItemMeta();
+	                    List<String> loreItem1 = new ArrayList<String>();
+	                    if (im.hasLore()) {
+	                    	loreItem1 = im.getLore();
+	                    } 
+	                    	loreItem1.add(ChatColor.translateAlternateColorCodes('&', getConfig().getString("lore")));
+		                    im.setLore(loreItem1);
+		                    p.getItemInHand().setItemMeta(im);
 	                }else {
 	                    sender.sendMessage("dont have permission!");
 	                    return true;
@@ -66,12 +82,12 @@ public class main extends JavaPlugin implements Listener,CommandExecutor{
 		      if ((entity instanceof Player)) {
 		        Player p = (Player)entity;
 		        
-		        for (int i = 0; i < 9; i++) {
+		        for (int i = 0; i < 10; i++) {
 		          if ((e.getInventory().getItem(i) != null) && 
 		            (e.getInventory().getItem(i).hasItemMeta()) && 
-		            (e.getInventory().getItem(i).getItemMeta().getLore().contains(ColorLore))) {
+		            (e.getInventory().getItem(i).getItemMeta().getLore().contains(getConfig().getString("lore").replace("&", "§")))) {
 		            e.getInventory().setResult(air);
-		            p.sendMessage(MsgColor);
+		            p.sendMessage(getConfig().getString("prefix").replace("&", "§") + (" ") + getConfig().getString("message.craft").replace("&", "§"));
 		          }
 		        }
 		      }
@@ -85,9 +101,9 @@ public class main extends JavaPlugin implements Listener,CommandExecutor{
 		    if ((event.getItem() != null) && 
 				(event.getItem().getItemMeta() != null) && 
 				(event.getItem().getItemMeta().getLore() != null) && 
-				(event.getItem().getItemMeta().getLore().contains(ColorLore))){
+				(event.getItem().getItemMeta().getLore().contains(getConfig().getString("lore").replace("&", "§")))){
 		      event.setCancelled(true);
-		      p.sendMessage(msgconsumeColor);
+		      p.sendMessage(getConfig().getString("prefix").replace("&", "§")  + (" ") +  getConfig().getString("message.consume").replace("&", "§"));
 		    }
 		  }
 		  
@@ -99,10 +115,10 @@ public class main extends JavaPlugin implements Listener,CommandExecutor{
 		    ((event.getItemInHand() != null) && 
 			(event.getItemInHand().hasItemMeta()) && 
 			(event.getItemInHand().getItemMeta().hasLore()) && 
-			(event.getItemInHand().getItemMeta().getLore().contains(ColorLore)))
+			(event.getItemInHand().getItemMeta().getLore().contains(getConfig().getString("lore").replace("&", "§"))))
 		    {
 		      event.setCancelled(true);
-		      p.sendMessage(msguseeColor);
+		      p.sendMessage(getConfig().getString("prefix").replace("&", "§")  + (" ") +  getConfig().getString("message.use").replace("&", "§"));
 		    }
 		  }
 		  
@@ -113,10 +129,10 @@ public class main extends JavaPlugin implements Listener,CommandExecutor{
 		    if ((event.getItem() != null) && 
 		      (event.getItem().getItemMeta() != null) && 
 		      (event.getItem().getItemMeta().getLore() != null) && 
-		      (event.getItem().getItemMeta().getLore().contains(ColorLore)))
+		      (event.getItem().getItemMeta().getLore().contains(getConfig().getString("lore").replace("&", "§"))))
 		    {
 		      event.setCancelled(true);
-		      p.sendMessage(msguseeColor);
+		      p.sendMessage(getConfig().getString("prefix").replace("&", "§")  + (" ") +  getConfig().getString("message.use").replace("&", "§"));
 		    }
 		  }
 }
